ee37afdfb74fe4a43251d02e6181829721ade149
Ákos Maróy <akos@maroy.hu>
2013-05-23 11:21:34 +0000

    added OamViewer class

---
+++ dojo-config.js
@@ -0,0 +1,54 @@
+var dojoConfig = {
+    baseUrl : 'lib',
+
+    // debug config for always refresh
+    urlArgs: "v=" +  (new Date()).getTime(),
+
+    waitSeconds : 0,
+
+    packages : [{
+        name : 'dojo',
+        location : 'ThirdParty/dojo-release-1.8.3-src/dojo'
+    }, {
+        name : 'dijit',
+        location : 'ThirdParty/dojo-release-1.8.3-src/dijit'
+    }, {
+        name : 'Assets',
+        location : 'Cesium/Assets'
+    }, {
+        name : 'Core',
+        location : 'Cesium/Core'
+    }, {
+        name : 'DynamicScene',
+        location : 'Cesium/DynamicScene'
+    }, {
+        name : 'Renderer',
+        location : 'Cesium/Renderer'
+    }, {
+        name : 'Scene',
+        location : 'Cesium/Scene'
+    }, {
+        name : 'Shaders',
+        location : 'Cesium/Shaders'
+    }, {
+        name : 'ThirdParty',
+        location : 'Cesium/ThirdParty'
+    }, {
+        name : 'Widgets',
+        location : 'Cesium/Widgets'
+    }, {
+        name : 'Dojo',
+        location : 'Cesium/Widgets/Dojo'
+    }, {
+        name : 'Workers',
+        location : 'Cesium/Workers'
+    }, {
+        name : 'Cesium',
+        location : 'Cesium'
+
+    }, {
+        name : 'OpenAviationMap',
+        location : 'OpenAviationMap'
+    }] 
+};
+
--- main.js
+++ main.js
@@ -1,50 +1,12 @@
-require({
-    baseUrl : 'lib',
-
-    // debug config for always refresh
-    urlArgs: "v=" +  (new Date()).getTime(),
-
-    waitSeconds : 0,
-
-    packages : [{
-        name : 'Assets',
-        location : 'Cesium/Assets'
-    }, {
-        name : 'Core',
-        location : 'Cesium/Core'
-    }, {
-        name : 'DynamicScene',
-        location : 'Cesium/DynamicScene'
-    }, {
-        name : 'Renderer',
-        location : 'Cesium/Renderer'
-    }, {
-        name : 'Scene',
-        location : 'Cesium/Scene'
-    }, {
-        name : 'Shaders',
-        location : 'Cesium/Shaders'
-    }, {
-        name : 'ThirdParty',
-        location : 'Cesium/ThirdParty'
-    }, {
-        name : 'Widgets',
-        location : 'Cesium/Widgets'
-    }, {
-        name : 'Workers',
-        location : 'Cesium/Workers'
-    }, {
-        name : 'Cesium',
-        location : 'Cesium'
-
-    }, {
-        name : 'OamViewer',
-        location : 'OpenAviationMap/OamViewer'
-    }] 
-}, [    
+require([    
     'Cesium/Cesium',
-    'OpenAviationMap/OamViewer'
-], function(Cesium, OamViewer) {
+    'Widgets/Dojo/CesiumViewerWidget',
+    'dojo/io-query',
+    'OpenAviationMap',
+], function(Cesium,
+            CesiumViewerWidget,
+            ioQuery,
+            OpenAviationMap) {
 
     // set up an OAM viewer and load a few AIXM files
     var aixmUrls = [ 'var/hungary-5.1.aixm51',
@@ -52,7 +14,7 @@ require({
                      'var/hungary-5.5.aixm51',
                      'var/hungary-5.6.aixm51' ];
 
-    var widget = new OamViewer({ divName : 'cesiumContainer', aixmUrls : aixmUrls });
+    var widget = new OpenAviationMap.OamViewer({ divName : 'cesiumContainer', aixmUrls : aixmUrls });
 
     // look at Hungary
     var cc = widget.cesiumWidget.scene.getCamera().controller;
